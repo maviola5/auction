99019e449d1caa8f03da210eb17040c8852d313a
Tony Viola <maviola5@gmail.com>
2016-12-05 22:10:25 -0500

    finished chapter 3

--- app/components/home/home.ts
+++ app/components/home/home.ts
@@ -4,7 +4,7 @@ import { Product, ProductService } from '../../services/product-service';
 @Component({
   selector: 'auction-home-page',
   styleUrls: ['/home.css'],
-  templateUrl: '.home.html'
+  templateUrl: 'app/components/home/home.html'
 })
 export default class HomeComponent {
   products: Product[] = [];
